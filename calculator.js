3e08e606dee9bfafa0a99e3785599ca382dc4f70
JustinSingleton <jsingle9@msudenver.edu>
2021-03-20 12:39:15 -0600

    post action added

--- calculator.js
+++ calculator.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) =>{
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post('/', (req, res) =>{
+  res.send("Post posted");
+});
+
 app.listen(port, ()=>{
   console.log("server running press ^C to stop");
 });
