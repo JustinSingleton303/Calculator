5daa50292927453cace1802bba543298c749d234
JustinSingleton <jsingle9@msudenver.edu>
2021-03-20 13:12:17 -0600

    body-parser added

--- calculator.js
+++ calculator.js
@@ -1,13 +1,17 @@
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) =>{
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post('/', (req, res) =>{
+  console.log(req.body);
   res.send("Post posted");
 });
 
